3288f92b875da28e888732ed27679b493ef0353c
HelloElwin <helloelwin@qq.com>
2022-09-30 12:45:24 +0800

    prepared for submit

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ if args.eval:
     tst_data = np.swapaxes(tst_data, -1, -2)
     tst_data = np.expand_dims(tst_data, axis=-1)
     tst_data *= 1000
-    model.load_weights('./ckpt/method2_best.ckpt')
+    model.load_weights('./ckpt/best.ckpt')
     probs = model.predict(tst_data)
     preds = probs.argmax(axis = -1)
     acc   = np.mean(preds == tst_label.argmax(axis=-1))
@@ -47,7 +47,7 @@ else:
     fittedModel = model.fit(trn_data, trn_label, batch_size = 16, epochs = 100, verbose = 2,
             validation_data=(tst_data, tst_label), callbacks=[checkpointer])
 
-    model.load_weights('./ckpt/method2_best.ckpt')
+    model.load_weights('./ckpt/best.ckpt')
 
     probs = model.predict(tst_data)
     preds = probs.argmax(axis = -1)
